f0889b5781b7444f6bac11ea3fa0918771ee38c5
maria-azurduy <mmaria.azurduy@gmail.com>
2025-08-23 17:00:08 -0300

    parte 1

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -13,7 +13,10 @@ longitud (x:xs) = 1 + longitud xs
 -- Dada una lista de enteros, devuelve la lista de los sucesores de cada entero.
 sucesores :: [Int] -> [Int]
 sucesores []     = []
-sucesores (x:xs) = sucesor x ++ sucesores xs 
+sucesores (x:xs) = sucesor x : sucesores xs 
+
+sucesor :: Int -> Int
+sucesor n = n + 1
 
 -- Dada una lista de booleanos devuelve True si todos sus elementos son True.
 conjuncion :: [Bool] -> Bool
@@ -31,43 +34,64 @@ disyuncion (b:bs) = esVerdadero b || disyuncion bs
 
 -- Dada una lista de listas, devuelve una única lista con todos sus elementos.
 aplanar :: [[a]] -> [a]
-aplanar [[]]     = []
-aplanar [(x:xs)] = 
+aplanar []      = []
+aplanar (xs:xss) = unirListas xs (aplanar xss)
+
+--
+unirListas :: [a] -> [a] -> [a] -- voy a recorrer SOLO la 1ra lista
+unirListas   []    ys = ys
+unirListas (x:xs)  ys = x : unirListas xs ys
+--
 
 -- Dados un elemento e y una lista xs devuelve True si existe un elemento en xs que sea igual a e.
 pertenece :: Eq a => a -> [a] -> Bool
-pertenece x []     =  
-pertenece x (y:ys) = 
+pertenece e []     =  False
+pertenece e (x:xs) =  e == x || pertenece e xs
 
 -- Dados un elemento e y una lista xs cuenta la cantidad de apariciones de e en xs.
 apariciones :: Eq a => a -> [a] -> Int
+apariciones e []     = 0
+apariciones e (x:xs) = if e == x then 1 + apariciones e xs else apariciones e xs
 
 -- Dados un número n y una lista xs, devuelve todos los elementos de xs que son menores a n.
 losMenoresA :: Int -> [Int] -> [Int]
+losMenoresA n []     =  []
+losMenoresA n (x:xs) =  if x < n then x : losMenoresA n xs else losMenoresA xs
 
 -- Dados un número n y una lista de listas, devuelve la lista de aquellas listas que tienen más de n elementos.
 lasDeLongitudMayorA :: Int -> [[a]] -> [[a]]
+lasDeLongitudMayorA n []      =  []
+lasDeLongitudMayorA n (xs:xss) = if longitud xs > n then xs : lasDeLongitudMayorA n xss else lasDeLongitudMayorA n xss
 
--- Dados una lista y un elemento, devuelve una lista con ese elemento agregado al ?nal de la lista.
+-- Dados una lista y un elemento, devuelve una lista con ese elemento agregado al final de la lista. TRIKYYYYYYY
 agregarAlFinal :: [a] -> a -> [a]
 agregarAlFinal [] x   = [x]
-agregarAlFinal (x:xs) = agregarAlFinal xs ++ x
+agregarAlFinal (x:xs) = x : agregarAlFinal xs 
 
--- Dadas dos listas devuelve la lista con todos los elementos de la primera lista y todos los elementos de la segunda a continuación. De?nida en Haskell como (++).
+-- Dadas dos listas devuelve la lista con todos los elementos de la primera lista y todos los elementos de la segunda a continuación. Definida en Haskell como (++).
 agregar :: [a] -> [a] -> [a]
 agregar [] ys = [ys]
 agregar (x:xs) ys = x + agregar xs ys 
 
 -- Dada una lista devuelve la lista con los mismos elementos de atrás para adelante. Definida en Haskell como reverse.
 reversa :: [a] -> [a]
+reversa []     = []
+reversa (x:xs) = agregarAlFinal (reversa xs)    x
+                              -- mi lista    mi elemento
 
 -- Dadas dos listas de enteros, devuelve una lista donde el elemento en la posición n es el máximo entre el elemento n de la primera lista y de la segunda lista, teniendo en cuenta que
 -- las listas no necesariamente tienen la misma longitud.
 zipMaximos :: [Int] -> [Int] -> [Int]
+zipMaximos is []         = is
+zipMaximos [] js         = js
+zipMaximos (i:is) (j:js) = if i > j then i : zipMaximos is js else j : zipMaximos is js
 
--- Dada una lista devuelve el mínimo
+-- Dada una lista devuelve el mínimo -- obs: la lista NO puede ser vacía
 elMinimo :: Ord a => [a] -> a
+elMinimo (x:xs) = minimo x (elMinimo xs)
 
+minimo :: Ord a  => a -> a -> a
+minimo x y = if x > y then x else y
 
 -- 2. RECURSION SOBRE NUMEROS
 -- Defina las siguientes funciones utilizando recursión sobre números enteros, salvo que se indique lo contrario:
