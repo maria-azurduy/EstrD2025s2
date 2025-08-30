de12581cff7800d2a9a5d3d6a28c520b67e5a5b8
maria-azurduy <mmaria.azurduy@gmail.com>
2025-08-29 22:06:44 -0300

    avances 3

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -47,7 +47,7 @@ unirListas (x:xs)  ys = x : unirListas xs ys
 -- Dados un elemento e y una lista xs devuelve True si existe un elemento en xs que sea igual a e.
 pertenece :: Eq a => a -> [a] -> Bool
 pertenece e []     =  False
-pertenece e (x:xs) =  e == x || pertenece e xs
+pertenece e (x:xs) =  (e == x) || pertenece e xs
 
 -- Dados un elemento e y una lista xs cuenta la cantidad de apariciones de e en xs.
 apariciones :: Eq a => a -> [a] -> Int
@@ -82,6 +82,7 @@ reversa (x:xs) = agregarAlFinal (reversa xs)    x
 
 -- Dadas dos listas de enteros, devuelve una lista donde el elemento en la posición n es el máximo entre el elemento n de la primera lista y de la segunda lista, teniendo en cuenta que
 -- las listas no necesariamente tienen la misma longitud.
+-- RECORRO DOS ESTRUCTURAS AL MISMO TIEMPO 
 zipMaximos :: [Int] -> [Int] -> [Int]
 zipMaximos is []         = is
 zipMaximos [] js         = js
@@ -89,6 +90,7 @@ zipMaximos (i:is) (j:js) = if i > j then i : zipMaximos is js else j : zipMaximo
 
 -- Dada una lista devuelve el mínimo -- obs: la lista NO puede ser vacía
 elMinimo :: Ord a => [a] -> a
+elMinimo [x]    = x
 elMinimo (x:xs) = minimo x (elMinimo xs)
 
 minimo :: Ord a  => a -> a -> a
@@ -147,14 +149,14 @@ mayoresA _ []     = []                                                         -
 mayoresA n (p:ps) = if edad p > n then p: mayoresA n ps else mayoresA n ps    
 
 -- Dada una lista de personas devuelve el promedio de edad entre esas personas. Precondición: la lista al menos posee una persona.
-promedioEdad :: [Persona] -> Int 
-promedioEdad ps  = div ((sumatoria edadesDe ps) longitud ps)
+promedioEdad :: [Persona]-> Int 
+promedioEdad ps = div (sumatoria (edades ps)) (longitud ps)
 
--- Dada una lista de personas devuelve la sumatoria de edad entre esas personas. Precondición: la lista al menos posee una persona.
-edadesDe :: [Persona] -> Int 
-edadesDe []     = error "La lista no puede ser vacia"
-edadesDe [p]    = [edad p]
-edadesDe (p:ps) = edad p + edadesDe ps 
+edades :: [Persona] -> [Int]                --Dada una lista de personas, devuelve una lista con las edades de esas personas
+                                            -- Precondición: la lista al menos posee una persona
+edades []       = error "La lista no puede ser vacia"
+edades [p]      = [edad p]
+edades (p:ps)   = edad p : edades ps
 
 -- Dada una lista de personas devuelve la persona más vieja de la lista. Precondición: la lista al menos posee una persona.
 elMasViejo :: [Persona] -> Persona
@@ -203,14 +205,18 @@ cantPokemon (E _ ps) = longitud ps
 
 -- Devuelve la cantidad de Pokémon de determinado tipo que posee el entrenador. 2
 cantPokemonDe :: TipoDePokemon -> Entrenador -> Int
-cantPokemonDe tipo (E _ ps) =  cantPokemonDeP tipo ps
+cantPokemonDe tipo (E _ ps) =  cantPokemonDeT tipo ps
 
-cantPokemonDeP :: TipoDePokemon -> [Pokemon] -> Int
-cantPokemonDeP _      []   = 0 
-cantPokemonDeP tipo (p:ps) = unoSiEsDeTipo (tipo p) + cantPokemonDeP tipo ps
+cantPokemonDeT :: TipoDePokemon -> [Pokemon] -> Int
+cantPokemonDeT _      []   = 0 
+cantPokemonDeT tipo (p:ps) = unoSiEsDeTipo tipo (tipoDe p) + cantPokemonDeT tipo ps
 
-unoSiEsDeTipo :: TipoDePokemon -> Pokemon -> Int
-unoSiEsDeTipo tipo p = if tipo == tipoDe p then 1 else 0
+unoSiEsMismoTipo :: TipoDePokemon -> TipoDePokemon -> Int
+unoSiEsMismoTipo Agua Agua     = 1
+unoSiEsMismoTipo Fuego Fuego   = 1
+unoSiEsMismoTipo Planta Planta = 1
+unoSiEsMismoTipo    _     _    = 0
+
 
 --
 tipoDe :: Pokemon -> TipoDePokemon -- Devuelve el tipo del pokemon 
@@ -223,12 +229,13 @@ cuantosDeTipo_De_LeGananATodosLosDe_ tipo (E _ ps1) (E _ ps2) = cuantosDeTipo_En
 
 cuantosDeTipo_En_LeGananA :: TipoDePokemon ->  [Pokemon] -> [Pokemon] -> Int
 cuantosDeTipo_En_LeGananA tipo []     _   = 0
-cuantosDeTipo_En_LeGananA tipo xs     []  = cantPokemonDe tipo xs 
+cuantosDeTipo_En_LeGananA tipo xs     []  = cantPokemonDeT tipo xs 
 cuantosDeTipo_En_LeGananA tipo (x:xs) ys  = if tipoDe x == tipo --aca filtro ya el tipo! 
                                             then unoSiLeGana_ATodos x ys + cuantosDeTipo_En_LeGananA tipo xs ys 
                                             else cuantosDeTipo_En_LeGananA tipo xs ys
 
 unoSiLeGana_ATodos :: Pokemon -> [Pokemon] -> Int
+unoSiLeGana_ATodos _ []     =  1
 unoSiLeGana_ATodos x (y:ys) =  unoSi_SuperaA_ x y * unoSiLeGana_ATodos tipo x ys
 
 unoSi_SuperaA_ :: Pokemon -> Pokemon -> Int
@@ -250,7 +257,7 @@ hayTodosLosTiposDePokemonesEn [] = False
 hayTodosLosTiposDePokemonesEn ps = hayPokemonDeTipo Agua ps && hayPokemonDeTipo Fuego ps && hayPokemonDeTipo Planta ps
 
 hayPokemonDeTipo ::  TipoDePokemon ->  [Pokemon] -> Bool
-hayPokemonDeTipo tipo (p:ps) = tipoDe p == tipo || hayPokemonDeTipo tipo ps
+hayPokemonDeTipo tipo (p:ps) = tipo == (tipoDe p)  || hayPokemonDeTipo tipo ps
 {-
     3.3
     El tipo de dato Rol representa los roles (desarollo o management) de empleados IT dentro
@@ -283,18 +290,25 @@ empresa = EM [rol1, rol2]
 
 -- Dada una empresa denota la lista de proyectos en los que trabaja, sin elementos repetidos.
 proyectos :: Empresa -> [Proyecto]
-proyectos (EM rs) = proyectosDe rs
+proyectos (EM rs) = proyectosSinRepetidos rs
 
-proyectosDe :: [Rol] -> [Proyecto] 
-proyectosDe []     = []
-proyectosDe (r:rs) = agregarAProyectoDe_SiNoEstaEn (proyectoDeRol r) (proyectosDe rs)
+proyectosSinRepetidos :: [Rol] -> [Proyecto] 
+proyectosSinRepetidos []     = []
+proyectosSinRepetidos (r:rs) = agregarProyectoSiNoEstaEn (proyectoDe r) (proyectosSinRepetidos rs)
 
-agregarAProyectoDe_SiNoEstaEn :: Rol -> [Proyecto] -> [Proyecto]
-agregarAProyectoDe_SiNoEstaEn r  ps = if pertenece (proyectoDeRol r ps) then [] else r:[]
+agregarProyectoSiNoEstaEn :: Proyecto -> [Proyecto] -> [Proyecto]
+agregarProyectoSiNoEstaEn p  ps = if proyectoPerteneceALaLista p ps  then ps else p : ps
 
 proyectoDe :: Rol -> Proyecto  -- Dado un rol, devuelve el proyecto
 proyectoDe (Developer _ p) = p
 proyectoDe (Management _ p) = p
+
+proyectoPerteneceALaLista :: Proyecto -> [Proyecto] -> Bool
+proyectoPerteneceALaLista _ [] = False
+proyectoPerteneceALaLista x (n:ns) =  (nombreProy x)== (nombreProy n) || (proyectoPerteneceALaLista x ns)
+
+nombreProy :: Proyecto -> String
+nombreProy (ConsProyecto n) = n
 
 -- Dada una empresa indica la cantidad de desarrolladores senior que posee, que pertecen además a los proyectos dados por parámetro.
 losDevSenior :: Empresa -> [Proyecto] -> Int
@@ -302,23 +316,53 @@ losDevSenior (EM rs) ps = devSeniorDe_QueTrabajaronEn rs ps
 
 devSeniorDe_QueTrabajaronEn :: [Rol] -> [Proyecto] -> Int
 devSeniorDe_QueTrabajaronEn [] _      = 0
-devSeniorDe_QueTrabajaronEn _ []      = 0
-devSeniorDe_QueTrabajaronEn (r:rs) ps = if esRolSeniorYTrabajoEn r ps 
-                                        then 1 + devSeniorDe_QueTrabajaronEn rs ps 
-                                        else devSeniorDe_QueTrabajaronEn rs ps 
+devSeniorDe_QueTrabajaronEn (r:rs) ps = unoSiEsDevSeniorYSuProyectoPerteneceA (seniority r) (proyectoDe r) ps + cantidadDevSeniorYDeProyectos rs ps
 
+unoSiEsDevSeniorYSuProyectoPerteneceA :: Seniority -> Proyecto -> [Proyecto] -> Int
+unoSiEsDevSeniorYSuProyectoPerteneceA Senior p ps = unoSiSuProyectoPerteneceA p ps 
+unoSiEsDevSeniorYSuProyectoPerteneceA _      _ _  = 0
+
+unoSiSuProyectoPerteneceA :: Proyecto -> [Proyecto] -> Int 
+unoSiSuProyectoPerteneceA p ps = if proyectoPerteneceALaLista p ps then 1 else 0
+
+seniority :: Rol -> Seniority 
+seniority (Developer s _) = s
+seniority (Management s _) = s
 
 -- Indica la cantidad de empleados que trabajan en alguno de los proyectos dados.
 cantQueTrabajanEn :: [Proyecto] -> Empresa -> Int
 cantQueTrabajanEn ps (EM rs) = cantEmpleadosDe_QueTrabajanEnAlgun ps rs 
 
 cantEmpleadosDe_QueTrabajanEnAlgun :: [Proyecto] -> [Rol] -> Int
-cantEmpleadosDe_QueTrabajanEnAlgun [] _      = 0
 cantEmpleadosDe_QueTrabajanEnAlgun _ []      = 0
-cantEmpleadosDe_QueTrabajanEnAlgun ps (r:rs) = if esEmpleado_QueTrabajoEnAlgun r ps 
-                                               then 1 + cantEmpleadosDe_QueTrabajanEnAlgun ps rs 
-                                               else cantEmpleadosDe_QueTrabajanEnAlgun ps rs 
+cantEmpleadosDe_QueTrabajanEnAlgun ps (r:rs) = unoSiTrabajaEnEnAlgunProyecto ps r + cantQueTrabajan ps rs
+
+unoSiTrabajaEnEnAlgunProyecto :: [Proyecto] -> Rol -> Int
+unoSiTrabajaEnEnAlgunProyecto ps (Developer _ p)  = unoSiSuProyectoPerteneceA p ps
+unoSiTrabajaEnEnAlgunProyecto ps (Management _ p) = unoSiSuProyectoPerteneceA p ps
 
 -- Devuelve una lista de pares que representa a los proyectos (sin repetir) junto con su cantidad de personas involucradas.
-{- asignadosPorProyecto :: Empresa -> [(Proyecto, Int)]
-asignadosPorProyecto (EM rs) =  -}
+asignadosPorProyecto :: Empresa -> [(Proyecto, Int)]
+asignadosPorProyecto (EM rs) =  cantPorProyecto rs
+
+cantPorProyecto :: [Rol] -> [(Proyecto, Int)]
+cantPorProyecto []      = []
+cantPorProyecto (r:rs)  = agregarATupla r (cantPorProyecto rs)
+
+agregarATupla :: Rol -> [(Proyecto, Int)] -> [(Proyecto, Int)] 
+agregarATupla (Developer _ p)  ps = sumarUnoEnTuplaCorrespondiente p ps
+agregarATupla (Management _ p) ps = sumarUnoEnTuplaCorrespondiente p ps
+
+
+sumarUnoEnTuplaCorrespondiente :: Proyecto -> [(Proyecto, Int)] -> [(Proyecto, Int)] 
+sumarUnoEnTuplaCorrespondiente p   []   = [(p, 1)] -- si llega a esto es xq la tupla no existe, por lo que tiene que crearla de cero
+sumarUnoEnTuplaCorrespondiente p (t:ts) = if esLaTupla p t 
+                                            then sumarUnoA t : ts 
+                                            else t : sumarUnoEnTuplaCorrespondiente p ts
+
+esLaTupla :: Proyecto -> (Proyecto, Int) -> Bool
+esLaTupla p1 (p2, _) = nombreProy p1  == nombreProy p2
+
+sumarUnoA :: (Proyecto, Int) -> (Proyecto, Int) 
+sumarUnoA (p , n) = (p, n+1)
+
